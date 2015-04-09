93b9f472629d6cfe1c8dc0c01744bacd312535de
Jacob Peterson <JacobAir@Jacobs-MacBook-Air.local>
2015-04-09 15:49:47 -0600

    minor bug fixes

--- app/app.js
+++ app/app.js
@@ -2,77 +2,80 @@ $(document).ready(function() {
 
 	$("#newTaskForm").hide();
 
-	var listo = [];
+var listo = [];
 
-	var Task = function() {
-		this.task = task;
-		this.id = "new";
-	};
+var Task = function(task) {
+   this.task = task;
+   this.id = 'new';
+}
 
-	var addTask = function(tast) {
-		if(task) {
-			task = new Task(task);
-			listo.push(task);
+var addTask = function(task) {
+    if(task) {
+        task = new Task(task);
+        listo.push(task);
 
-			$("#newItemInput").val("");
-			$("#newList").append("<a href='#finish' class= '' id='item'><li class='list-group-item'>" + task.task + "<span class='arrow pull-right'><i class='glyphicon glyphicon-arrow-right'></span></li></a>");
+        $('#newItemInput').val('');
 
-		}
-		$("#newTaskForm, #newListItem").fadeToggle("fast", "linear");
-	};
+        $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
 
-	var advanceTask = function(task) {
-		var modified = task.innerText.trim();
-		for (var i = 0; i < listo.lenght; i++) {
-			if (listo[i] === modified) {
-				if(listo[i].id === "new") {
-					listo[i].id = "inProgress";
-				} else if (listo[i].id === "inProgress") {
-					listo[i].id="archived";
-				} else {
-					listo.splice(i,1);
-				}
-				break;
+    }
+    $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+
+};
+
+var advanceTask = function(task) {
+	var modified = task.innerText.trim();
+	for (var i = 0; i < listo.lenght; i++) {
+		if (listo[i] === modified) {
+			if(listo[i].id === "new") {
+				listo[i].id = "inProgress";
+			} else if (listo[i].id === "inProgress") {
+				listo[i].id="archived";
+			} else {
+				listo.splice(i,1);
 			}
+			break;
 		}
-		task.remove();
-	};
+	}
+	task.remove();
+};
 
-	$("#saveNewItem").on("click", function(e) {
-		e.preventDefault();
-		var task = $("#newItemInput").val().trim();
-		addTask(task);
-	});
+$('#saveNewItem').on('click', function (e) {
+    e.preventDefault();
+    var task = $('#newItemInput').val().trim();
+    addTask(task);
+});
 
-	$("#newListItem").on("click", function() {
-		$("#newTaskForm, #newListItem").fadeToggle("fast", "linear");
-	});
+$('#newListItem').on('click', function () {
+    $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+});
 
-	$("#cancel").on("click", function(e) {
-		e.preventDefault();
-		$("#newTaskForm, #newListItem").fadeToggle("fast", "linear");
-	});
+$('#cancel').on('click', function (e) {
+    e.preventDefault();
+    $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+});
 
-	$(document).on("click", "#item", function(e) {
-		e.preventDefault();
-		var task = this;
-		advanceTask(task);
-		this.id = "inProgress";
-		$("#currentList").append(this.outerHTML);
-	});
+$(document).on('click', '#item', function(e) {
+    e.preventDefault();
+    var task = this;        
+    advanceTask(task);
+    this.id = 'inProgress';
+    $('#currentList').append(this.outerHTML);
 
-	$(document).on("click", "#inProgress", function(e) {
-		e.preventDefault();
-		var task = this;
-		task.id = "archived";
-		var changeIcon = task.outerHTML.replace("glyphicon-arrow-right", "glyphicon-remove");
-		advanceTask(task);
-		$("#archivedList").append(changeIcon);
-	});
+});
 
-	$(document).on("click", "#archived", function(e) {
-		e.preventDefault();
-		var task = this;
-		advanceTask(task);
-	});
+$(document).on('click', '#inProgress', function (e) {
+    e.preventDefault();
+    var task = this;
+    task.id = "archived";
+    var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+    advanceTask(task);
+    $('#archivedList').append(changeIcon);
+});
+
+$(document).on('click', '#archived', function (e) {
+    e.preventDefault();
+    var task = this;
+    advanceTask(task);
+});
 });
